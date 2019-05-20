d5c5feee1d8cd77d23909518ba075a461c15cbb3
goobering <goobering@gmail.com>
2019-05-20 12:40:22 +0100

    Initial commit

---
+++ Kata.Services/NewYearChaosService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Kata.Services
+{
+    public static class NewYorkChaosService
+    {
+        static int MinimumBribes(int[] q)
+        {
+            if(IsSorted(q))
+            {
+                return 0;
+            }
+
+            
+        }
+
+        static bool IsSorted(int[] arr)
+        {
+            for(int i = 1; i < arr.Length; i++)
+            {
+                if(arr[i - 1] > arr[i])
+                {
+                    return false;
+                }
+
+                return true;
+            }
+        }
+    }
+}
---
+++ Kata/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Kata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MinimumBribes(new int[]{1, 2, 3});
+        }
+
+        static void MinimumBribes(int[] q) 
+        {
+            Console.WriteLine("Some output.");
+        }
+    }
+}
